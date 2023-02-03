8c3c19f16a09edd7bbe13ed3d5d6c908f6931261
Olga Chernega <eliang.cheng@gmail.com>
2023-02-03 14:36:55 +0200

    feat: add team section

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { featuresData } from '../../utils/featuresData';
+import { teamMembers } from '../../utils/teamMembers';
 import Definition from './components/Definition/Definition';
 import Features from './components/Features/Features';
 import FullScreenBlock from './components/FullScreenBlock/FullScreenBlock';
+import TeamMembers from './components/TeamMembers/TeamMembers';
 
 export default function HomePage() {
   return (
@@ -10,6 +12,7 @@ export default function HomePage() {
       <FullScreenBlock />
       <Definition />
       <Features features={featuresData} />
+      <TeamMembers team={teamMembers} />
     </>
   );
 }
--- src/pages/HomePage/components/FullScreenBlock/FullScreenBlock.tsx
+++ src/pages/HomePage/components/FullScreenBlock/FullScreenBlock.tsx
@@ -1,14 +1,18 @@
+import { Link } from 'react-router-dom';
+
 export default function FullScreenBlock() {
   return (
     <div className="full-screen-block">
       <div className="full-screen-block__container">
         <h2 className="full-screen-block__title">TypeScript Developer Course</h2>
         <ul className="full-screen-block__list">
-          <li className="full-screen-block__list-item">TypeScript tasks</li>
-          <li className="full-screen-block__list-item">Online learning</li>
+          <li className="full-screen-block__list-item">TypeScript lessons</li>
+          <li className="full-screen-block__list-item">Online education</li>
           <li className="full-screen-block__list-item">Theory and practice</li>
         </ul>
-        <button className="full-screen-block__button">Start</button>
+        <Link to="/lessons" className="full-screen-block__button button">
+          Start Learning
+        </Link>
       </div>
     </div>
   );
