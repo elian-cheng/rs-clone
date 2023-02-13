718b42a5b4a8f52b7e6e1aa129c1b031820bed1c
Eugene <gradam2001@gmail.com>
2023-02-13 16:27:00 +0200

    fix: statistic charts logic

--- src/pages/StatisticsPage/components/ChartsBlock/DoughnutChart.tsx
+++ src/pages/StatisticsPage/components/ChartsBlock/DoughnutChart.tsx
@@ -9,7 +9,7 @@ export default function DoughnutChart({ done, total }: { done: number; total: nu
     datasets: [
       {
         label: 'Tasks',
-        data: [done, total],
+        data: [done, total - done],
         backgroundColor: ['rgba(251, 232, 166, 1)', 'rgba(58, 87, 168, 1)'],
         borderColor: ['rgba(251, 232, 166, 1)', 'rgba(58, 87, 168, 1)'],
         borderWidth: 1,
