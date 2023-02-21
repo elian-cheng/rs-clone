4ab985f842c13c01bd65a48486503b14b11908aa
Alexander_Yulin <ordinaraviro@gmail.com>
2023-02-21 22:41:06 +0200

    refactor: remove some comments

--- src/components/Sidebar/Burger/Burger.tsx
+++ src/components/Sidebar/Burger/Burger.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 type Props = {
   onClick: () => void;
@@ -6,29 +6,6 @@ type Props = {
 };
 
 export default function Burger(props: Props) {
-  // const burgerAction = (event: MouseEvent) => {
-  //   const burgerBtn = (event.target as HTMLElement).closest('.sidebar-burger');
-  //   if (burgerBtn || (event.target as HTMLElement).classList.contains('sidebar-wrapper_open')) {
-  //     document.querySelector('.sidebar-wrapper')?.classList.toggle('sidebar-wrapper_open');
-  //     document.querySelector('.sidebar-burger')?.classList.toggle('activebrgr');
-  //     document.body.classList.toggle('scroll-lock');
-  //   }
-  // }
-
-  // const [barOpen, setActive] = useState(false);
-  // const barToggle = () => {
-  //   setActive(!barOpen);
-  // };
-
-
-
-  // useEffect(() => {                                 
-  //   addEventListener('click', burgerAction);
-  //   return () => {
-  //     removeEventListener('click', burgerAction)
-  //   }
-  // }, [])
-
   return (
    <div onClick={props.onClick} className={props.className}>
       <div className="sidebar-burger__dash"></div>
