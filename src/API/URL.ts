ea224176dc09d1017af1e20916395044202a0031
Olga Chernega <eliang.cheng@gmail.com>
2023-05-22 16:04:05 +0300

    fix: change the back-end link

--- src/API/URL.ts
+++ src/API/URL.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = 'https://ts-academy.up.railway.app/';
+// export const BASE_URL = 'https://ts-academy.up.railway.app/';
+export const BASE_URL = 'https://ts-academy-elian-cheng.onrender.com/';
